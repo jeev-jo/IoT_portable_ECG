dcf0a54c6c4f1691af466f120be3767ab7b87f86
jeev-jo <jeevsspace@gmail.com>
2023-03-27 10:10:22 +0530

    plotting done

--- App/ecg_lead/app/src/main/java/com/example/ecg_lead/MainActivity.java
+++ App/ecg_lead/app/src/main/java/com/example/ecg_lead/MainActivity.java
@@ -10,26 +10,40 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebSocketClient mWebSocketClient;
-    //EditText res;
-    Button st;
+    private EditText res;
+    private Button st;
+    private LineChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        res = findViewById(R.id.res);
+        chart = findViewById(R.id.chart);
+        chart.setTouchEnabled(true);
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(true);
+        chart.setDrawMarkers(false);
+        chart.getDescription().setEnabled(false);
+
+        // Create a new LineData object
+        LineData data = new LineData();
+        chart.setData(data);
         st = findViewById(R.id.b);
         st.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,14 +52,12 @@ public class MainActivity extends AppCompatActivity {
                 connectWebSocket();
             }
         });
-
-
     }
 
     private void connectWebSocket() {
         URI uri;
         try {
-            uri = new URI("ws://192.168.43.114/ws");
+            uri = new URI("ws://192.168.43.189/ws");
         } catch (URISyntaxException e) {
             e.printStackTrace();
             return;
@@ -56,43 +68,45 @@ public class MainActivity extends AppCompatActivity {
             public void onOpen(ServerHandshake serverHandshake) {
                 Log.i("Websocket", "Opened");
                 mWebSocketClient.send("Hello from Android!");
+                res.setText("");
             }
 
-
+            @Override
             public void onMessage(String s) {
-                final String message = s;
-                runOnUiThread(() -> {
-                    // Inside the WebSocketClient.onMessage() method
-                    int dataPoint = Integer.parseInt(message);
-                    LineChart chart = findViewById(R.id.chart);
-                    //res = findViewById(R.id.res);
-                    //res.append(s);
-                    LineData lineData = chart.getData();
-                    if (lineData != null) {
-                        ILineDataSet dataSet = lineData.getDataSetByIndex(0);
-                        if (dataSet == null) {
-                            dataSet = createSet();
-                            lineData.addDataSet(dataSet);
-                        }
-                        dataSet.addEntry(new Entry(dataSet.getEntryCount(), dataPoint));
-                        lineData.notifyDataChanged();
-                        chart.notifyDataSetChanged();
-                        chart.setVisibleXRangeMaximum(120); // Adjust visible range to show last 120 entries
-                        chart.moveViewToX(lineData.getEntryCount()); // Move the view to the end of the data
+                // Convert the received value to a float
+                float value = Float.parseFloat(s);
+                // Get the chart's existing data and dataset
+                LineData data = chart.getData();
+                if (data != null) {
+                    LineDataSet set = (LineDataSet) data.getDataSetByIndex(0);
+                    if (set == null) {
+                        set = createSet();
+                        data.addDataSet(set);
                     }
-                });
+                    // Add the new value to the dataset and notify the chart of the update
+                    set.addEntry(new Entry(set.getEntryCount(), value));
+                    data.notifyDataChanged();
+                    chart.setDrawMarkers(true);
+                    chart.notifyDataSetChanged();
+                    // Scroll the chart to the right to show the latest data
+                    chart.setVisibleXRangeMaximum(200);
+                    chart.moveViewToX(data.getEntryCount() - 1);
+                }
             }
-
+            // Helper method to create a LineDataSet
             private LineDataSet createSet() {
-                LineDataSet set = new LineDataSet(null, "ECG Data");
-                set.setLineWidth(2.5f);
-                set.setColor(Color.BLUE);
+                LineDataSet set = new LineDataSet(null, "ECG");
+                set.setAxisDependency(YAxis.AxisDependency.LEFT);
+                set.setColor(Color.RED);
                 set.setDrawCircles(false);
                 set.setDrawValues(false);
+                set.setLineWidth(2f);
+                set.setHighlightEnabled(false);
+                set.setDrawHorizontalHighlightIndicator(false);
+                set.setFillAlpha(65);
+                set.setFillColor(Color.RED);
                 return set;
             }
-
-
 
             @Override
             public void onClose(int i, String s, boolean b) {
@@ -104,6 +118,7 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Websocket", "Error " + e.getMessage());
             }
         };
+
         mWebSocketClient.connect();
     }
 
---
+++ App/ecg_lead/app/src/main/java/com/example/ecg_lead/PongMessage.java
@@ -0,0 +1,2 @@
+package com.example.ecg_lead;public class PongMessage {
+}
