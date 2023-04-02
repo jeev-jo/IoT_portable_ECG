3481e5e00fec0b0168dccb6af70f51248b478182
jeev-jo <jeevsspace@gmail.com>
2023-04-02 15:28:24 +0530

    data now saves to pdf and csv

--- App/ecg_lead/app/src/main/java/com/example/ecg_lead/MainActivity.java
+++ App/ecg_lead/app/src/main/java/com/example/ecg_lead/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.ecg_lead;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -19,14 +21,20 @@ import com.github.mikephil.charting.data.LineDataSet;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebSocketClient mWebSocketClient;
     private EditText res;
-    private Button st;
+
     private LineChart chart;
 
     @Override
@@ -44,12 +52,19 @@ public class MainActivity extends AppCompatActivity {
         // Create a new LineData object
         LineData data = new LineData();
         chart.setData(data);
-        st = findViewById(R.id.b);
+        Button st = findViewById(R.id.b);
         st.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Connect to the WebSocket server
                 connectWebSocket();
+            }
+        });
+        Button rec = findViewById(R.id.record);
+        rec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveToPdf();
             }
         });
     }
@@ -91,8 +106,25 @@ public class MainActivity extends AppCompatActivity {
                     // Scroll the chart to the right to show the latest data
                     chart.setVisibleXRangeMaximum(200);
                     chart.moveViewToX(data.getEntryCount() - 1);
+
+                    // Save the ECG data to a CSV file with timestamp
+                    try {
+                        FileWriter writer = new FileWriter(getExternalFilesDir(null) + "/ecg_data.csv", true);
+                        String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+                        String csvString = timestamp + "," + value + "\n";
+                        writer.write(csvString);
+                        writer.flush();
+                        writer.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    // Add the timestamped value to the list of ecg data
+                    //EcgData ecgData = EcgData.getInstance();
+                    //ecgData.add(new EntryWithTimestamp(value, System.currentTimeMillis()));
                 }
             }
+
             // Helper method to create a LineDataSet
             private LineDataSet createSet() {
                 LineDataSet set = new LineDataSet(null, "ECG");
@@ -120,6 +152,43 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mWebSocketClient.connect();
+    }
+    private void saveToPdf() {
+        // Create a new PDF document
+        PdfDocument pdfDocument = new PdfDocument();
+
+        // Set up the page dimensions and attributes
+        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(
+                chart.getWidth(), chart.getHeight(), 1)
+                .create();
+
+        // Start a new page in the document
+        PdfDocument.Page page = pdfDocument.startPage(pageInfo);
+
+        // Draw the chart on the PDF page canvas
+        Canvas canvas = page.getCanvas();
+        chart.draw(canvas);
+
+        // Finish the page
+        pdfDocument.finishPage(page);
+
+        // Save the document to a file
+        String fileName = "ecg_data.pdf";
+        File file = new File(getExternalFilesDir(null), fileName);
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            pdfDocument.writeTo(outputStream);
+            outputStream.flush();
+            outputStream.close();
+            Toast.makeText(MainActivity.this,
+                    "PDF saved to " + file.getAbsolutePath(),
+                    Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Close the document
+        pdfDocument.close();
     }
 
     @Override
